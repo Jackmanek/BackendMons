a473f661e385b5d464a1bc8468ecf47c839ee9fd
Jasio <age002@gmail.com>
2025-07-09 12:50:44 +0200

    añadimos creacion jornadas y resultados

--- src/main/java/com/padelmons/PadelMons/PadelMonsApplication.java
+++ src/main/java/com/padelmons/PadelMons/PadelMonsApplication.java
@@ -26,6 +26,42 @@ public class PadelMonsApplication implements CommandLineRunner {
 		return env -> {
 
 
+			Player player0 = new Player("Carlos", "Palomares", 52, "Masculino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+			Player player1 = new Player("Jasmany", "Zambrano", 37, "Masculino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+			Player player2 = new Player("Oscar", "Colmenero", 38, "Masculino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+			Player player3 = new Player("Alberto", "Bueno", 52, "Masculino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+			Player player4 = new Player("Luis", "Trejo", 52, "Masculino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+			Player player5 = new Player("Joseba", "Sanchez", 25, "Masculino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+			Player player6 = new Player("Mikel", "Virceda", 38, "Masculino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+			Player player7 = new Player("Sofia", "Araujo", 24, "Femenino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+			Player player8 = new Player("Antonia", "Tapia", 29, "Femenino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+			Player player9 = new Player("Ohiana", "Gordobil", 24, "Femenino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+			Player player10 = new Player("Maria", "Arrieta", 31, "Femenino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+			Player player11 = new Player("Pedro", "Caryon", 32, "Masculino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+			Player player12 = new Player("Jon", "Baryolo", 27, "Masculino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+			Player player13 = new Player("Adrian", "Cohello", 20, "Masculino", "", new DataContact("carlos@hotmail.com","123456789"),new Team(""));
+
+
+			playerRepository.save(player0);
+			playerRepository.save(player1);
+			playerRepository.save(player2);
+			playerRepository.save(player3);
+			playerRepository.save(player4);
+			playerRepository.save(player5);
+			playerRepository.save(player6);
+			playerRepository.save(player7);
+			playerRepository.save(player8);
+			playerRepository.save(player9);
+			playerRepository.save(player10);
+			playerRepository.save(player11);
+			playerRepository.save(player12);
+			playerRepository.save(player13);
+
+
+
+
+
+
 
 			Role r = new Role(ERole.ROLE_USER);
 			Role r1 = new Role(ERole.ROLE_MODERATOR);
