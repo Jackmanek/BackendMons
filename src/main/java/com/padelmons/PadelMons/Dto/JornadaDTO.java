cdb39787dd741bf0ea8bac388718f81a173e8433
Jasio <age002@gmail.com>
2025-07-15 16:32:47 +0200

    mejoras modificacion

--- src/main/java/com/padelmons/PadelMons/Dto/JornadaDTO.java
+++ src/main/java/com/padelmons/PadelMons/Dto/JornadaDTO.java
@@ -3,4 +3,25 @@ package com.padelmons.PadelMons.Dto;
 public class JornadaDTO {
     public int numJornada;
     public Long faseId;
+
+    public JornadaDTO(int numJornada, Long faseId) {
+        this.numJornada = numJornada;
+        this.faseId = faseId;
+    }
+
+    public int getNumJornada() {
+        return numJornada;
+    }
+
+    public void setNumJornada(int numJornada) {
+        this.numJornada = numJornada;
+    }
+
+    public Long getFaseId() {
+        return faseId;
+    }
+
+    public void setFaseId(Long faseId) {
+        this.faseId = faseId;
+    }
 }
